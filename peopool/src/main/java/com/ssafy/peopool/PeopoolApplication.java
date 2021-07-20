549eae316d01f3cc9aff4155025ba34e01f7119a
DESKTOP-KVCQHCD\multicampus <shchae.dev@gmail.com>
2021-07-20 10:28:50 +0900

    초기 프로텍트 생성

---
+++ peopool/src/main/java/com/ssafy/peopool/PeopoolApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.peopool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopoolApplication.class, args);
+	}
+
+}
