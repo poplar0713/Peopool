58f403cb2a1fd652600f1fcd2bd0613b5f84e31e
DESKTOP-KVCQHCD\multicampus <shchae.dev@gmail.com>
2021-07-20 10:47:06 +0900

    초기 프로젝트 재생성

--- peopool/src/main/java/com/ssafy/peopool/PeopoolApplication.java
+++ peopool/src/main/java/com/ssafy/peopool/PeopoolApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PeopoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PeopoolApplication.class, args);
+		SpringApplication.run(PeopoolApplication.class, args); 
 	}
 
 }
