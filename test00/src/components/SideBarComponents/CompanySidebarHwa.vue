aa0fb3e002d6d936d6626550568bb03e0b4ccb30
mooonmovie <sosm8500@gmail.com>
2021-07-21 14:51:03 +0900

    Layout Container

--- test00/src/components/SideBarComponents/CompanySidebarHwa.vue
+++ test00/src/components/SideBarComponents/CompanySidebarHwa.vue
@@ -1,73 +1,61 @@
 <template>
-  <el-container>
-    <el-aside class="sidebar" width="250px">
-      <router-link to="/home">
-        <div class="title">
-          <div>
-            <span class="f">P</span>
-            <span class="s">eo</span>
-            <span class="s">P</span>
-            <span class="f">ool</span>
-          </div>
-        </div>
-      </router-link>
+  <el-menu
+    class="el-menu-vertical-demo"
+    background-color="#F4F6F6"
+    text-color="#fff"
+    active-text-color="#FFC000"
+  >
+    <el-menu-item index="1">
+      <i class="el-icon-menu"></i>
+      <span>공고관리</span>
+    </el-menu-item>
 
-      <el-menu
-        class="el-menu-vertical-demo"
-        background-color="#F4F6F6"
-        text-color="#fff"
-        active-text-color="#FFC000"
-      >
-        <el-menu-item index="1">
-          <i class="el-icon-menu"></i>
-          <span>공고관리</span>
-        </el-menu-item>
+    <el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <span>면접관리</span>
+    </el-menu-item>
 
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span>면접관리</span>
-        </el-menu-item>
+    <el-menu-item index="3">
+      <i class="el-icon-document"></i>
+      <span>관심 피플인</span>
+    </el-menu-item>
 
-        <el-menu-item index="3">
-          <i class="el-icon-document"></i>
-          <span>관심 피플인</span>
-        </el-menu-item>
-
-        <el-menu-item index="4" disabled>
-          <i class="el-icon-setting"></i>
-          <span>settings</span>
-        </el-menu-item>
-      </el-menu>
-    </el-aside>
-
-    <!-- <router-view></router-view> -->
-  </el-container>
+    <el-menu-item index="4" disabled>
+      <i class="el-icon-setting"></i>
+      <span>settings</span>
+    </el-menu-item>
+  </el-menu>
+  <!-- <router-view></router-view> -->
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@1,700&display=swap");
+
 .sidebar {
   position: fixed;
   height: 100%;
   overflow: auto;
 }
 .el-menu {
-  height: 90%;
+  height: 100%;
 }
+
 span {
   color: black;
 }
 .title {
+  font-family: "Work Sans", sans-serif;
   font-size: 40px;
   text-decoration: none;
   /* padding: 10px; */
-  display: flex;
+  /* display: flex;
   align-content: center;
   justify-content: center;
-  height: 10%;
+  height: 10%; */
 }
 
 .f {
