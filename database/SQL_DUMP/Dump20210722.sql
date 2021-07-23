72c2c74f0ab9fa96a2e7763dfec9c998d741b2fb
허창환 <ckdghks34@gmail.com>
2021-07-23 11:00:31 +0900

    DB연결

--- database/SQL_DUMP/Dump20210722.sql
+++ database/SQL_DUMP/Dump20210722.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
--- Host: localhost    Database: test2
+-- Host: localhost    Database: peopool
 -- ------------------------------------------------------
 -- Server version	5.7.33-log
 
