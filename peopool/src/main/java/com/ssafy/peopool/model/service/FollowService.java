e12c700dc73df2d49da8f04ef04688c189029e48
DESKTOP-KVCQHCD\multicampus <shchae.dev@gmail.com>
2021-07-26 10:40:27 +0900

    팔로우 조회 오류 해결

--- peopool/src/main/java/com/ssafy/peopool/model/service/FollowService.java
+++ peopool/src/main/java/com/ssafy/peopool/model/service/FollowService.java
@@ -16,14 +16,14 @@ public interface FollowService {
 	boolean deleteFollow(Follow follow) throws SQLException;
 
 	// 팔로워 숫자 조회
-	int getFollowerCount(Map<String, String> map) throws SQLException;
+	int getFollowerCount(String index, String type) throws SQLException;
 	
-	int getFollowingCount(Map<String, String> map) throws SQLException;
+	int getFollowingCount(String index, String type) throws SQLException;
 
 	// 팔로워 조회
-	List<Follow> getFollowing(Map<String, String> map) throws SQLException;
+	List<Follow> getFollowing(String index, String type) throws SQLException;
 
 	// 팔로잉 조회
-	List<Follow> getFollower(Map<String, String> map) throws SQLException;
+	List<Follow> getFollower(String index, String type) throws SQLException;
 
 }
