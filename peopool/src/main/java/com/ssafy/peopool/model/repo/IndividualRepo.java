3423c8341c21b1106d9250e2e674a1cc20a457f3
DESKTOP-KVCQHCD\multicampus <shchae.dev@gmail.com>
2021-07-26 09:50:05 +0900

    기업, 개인 회원 아이디 비밀번호 찾기 오류 해결

--- peopool/src/main/java/com/ssafy/peopool/model/repo/IndividualRepo.java
+++ peopool/src/main/java/com/ssafy/peopool/model/repo/IndividualRepo.java
@@ -28,8 +28,8 @@ public interface IndividualRepo {
 	int deleteIndividual(int index) throws SQLException;
 
 	// 아이디 찾기
-	Individual findIndividualID(Individual individual) throws SQLException;
+	Individual findIndividualID(String name, String phone) throws SQLException;
 
 	// 비밀번호 찾기
-	Individual findIndividualPW(Individual individual) throws SQLException;
+	Individual findIndividualPW(String id, String phone) throws SQLException;
 }
