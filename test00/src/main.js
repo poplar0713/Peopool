a9ec63841462cfed3679dd5ff751e88e1999a7de
mooonmovie <sosm8500@gmail.com>
2021-07-26 09:45:05 +0900

    보류

--- test00/src/main.js
+++ test00/src/main.js
@@ -6,11 +6,11 @@ import installElementPlus from "./plugins/element";
 // import vueMoment from "vue-moment";
 // Vue.use(vueMoment);
 
-// kakaotalk
-window.Kakao.init("e5254ff19094b2f428ebe96dfb28c1a7");
-// firebase
-require("../node_modules/firebaseui/dist/firebaseui.js");
-require("../node_modules/firebaseui/dist/firebaseui.css");
+// // kakaotalk
+// window.Kakao.init("e5254ff19094b2f428ebe96dfb28c1a7");
+// // firebase
+// require("../node_modules/firebaseui/dist/firebaseui.js");
+// require("../node_modules/firebaseui/dist/firebaseui.css");
 
 const app = createApp(App);
 installElementPlus(app);
