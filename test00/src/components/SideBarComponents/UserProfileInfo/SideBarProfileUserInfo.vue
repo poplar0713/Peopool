49cfb378524038aa67012240d628cee895efc86f
조영우 <awesome_95@naver.com>
2021-08-03 00:30:43 +0900

    userprofile

--- test00/src/components/SideBarComponents/UserProfileInfo/SideBarProfileUserInfo.vue
+++ test00/src/components/SideBarComponents/UserProfileInfo/SideBarProfileUserInfo.vue
@@ -3,19 +3,11 @@
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
       <!-- 개인회원이름 -->
       <el-form-item label="Name" prop="UserName">
-        <el-input v-model="ruleForm.UserName">{{ this.ruleForm.UserName }}</el-input>
+        {{ this.ruleForm.UserName }}
       </el-form-item>
       <!-- 개인회원 생년월일 -->
       <el-form-item label="Birth" prop="UserBirth">
-        <el-input v-model="ruleForm.UserBirth" placeholder="YYMMDD형식으로 작성해주세요"></el-input>
-      </el-form-item>
-      <!-- 개인회원 PW -->
-      <el-form-item label="Password" prop="Password">
-        <el-input type="password" v-model="ruleForm.Password"></el-input>
-      </el-form-item>
-      <!-- 개인회원 PW 확인 -->
-      <el-form-item label="Password Confirmation" prop="PasswordConfirm">
-        <el-input type="password" v-model="ruleForm.PasswordConfirm"></el-input>
+        {{ this.ruleForm.UserBirth }}
       </el-form-item>
       <!-- 공개여부 -->
       <el-form-item label="Open to the public" prop="open">
@@ -23,10 +15,7 @@
       </el-form-item>
       <!-- 성별 -->
       <el-form-item label="Gender" prop="Gender">
-        <el-radio-group v-model="ruleForm.Gender">
-          <el-radio label="male"></el-radio>
-          <el-radio label="female"></el-radio>
-        </el-radio-group>
+        {{ this.ruleForm.Gender }}
       </el-form-item>
       <!-- 연락처 -->
       <el-form-item label="Tel" prop="UserTel">
@@ -35,6 +24,14 @@
       <!-- 이메일 -->
       <el-form-item label="Email" prop="UserEmail">
         <el-input type="email" v-model="ruleForm.UserEmail"></el-input>
+      </el-form-item>
+      <!-- 개인회원 PW -->
+      <el-form-item label="Password" prop="Password">
+        <el-input type="password" v-model="ruleForm.Password"></el-input>
+      </el-form-item>
+      <!-- 개인회원 PW 확인 -->
+      <el-form-item label="Password Confirmation" prop="PasswordConfirm">
+        <el-input type="password" v-model="ruleForm.PasswordConfirm"></el-input>
       </el-form-item>
       <div style="float:right">
         <el-form-item>
@@ -65,16 +62,26 @@ export default {
     axios
       .get(`/ind/${index}`)
       .then((res) => {
-        console.log(res.data.ind_name);
         this.ruleForm.UserName = res.data.ind_name;
         this.ruleForm.UserBirth = res.data.ind_birth;
         this.ruleForm.Gender = res.data.ind_gender;
         this.ruleForm.UserTel = res.data.ind_phone;
         this.ruleForm.UserEmail = res.data.ind_email;
+        this.ruleForm.UserIndex = res.data.ind_index;
+        this.ruleForm.UserId = res.data.ind_id;
       })
       .catch((err) => {
         console.log(err);
       });
+    const checkPWCF = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("비밀번호(확인)를 입력해주세요"));
+      } else if (value !== this.ruleForm.ind_password) {
+        callback(new Error("비밀번호를 확인해주세요"));
+      } else {
+        callback();
+      }
+    };
     return {
       ruleForm: {
         Password: "",
@@ -85,25 +92,27 @@ export default {
         Gender: "",
         UserTel: "",
         UserEmail: "",
+        UserId: "",
+        UserIndex: 0,
       },
       rules: {
-        SignupIndivID: [
+        Password: [
           {
             required: true,
-            message: "Please input Activity ID",
+            message: "비밀번호를 입력해주세요",
             trigger: "blur",
           },
           {
-            min: 5,
-            max: 10,
-            message: "Length should be 5 to 10",
+            min: 10,
+            max: 15,
+            message: "5-10자리 비밀번호를 입력해주세요",
             trigger: "blur",
           },
         ],
-        SignupIndivPW: [
+        PasswordConfirm: [
           {
             required: true,
-            message: "Please input Activity Password",
+            message: "비밀번호를 확인해주세요",
             trigger: "blur",
           },
           {
@@ -112,42 +121,7 @@ export default {
             message: "Length should be 10 to 15",
             trigger: "blur",
           },
-        ],
-        SignupIndivPWConfirm: [
-          { required: true, message: "Please check Password", trigger: "blur" },
-          {
-            min: 10,
-            max: 15,
-            message: "Length should be 10 to 15",
-            trigger: "blur",
-          },
-        ],
-        UserName: [
-          {
-            required: true,
-            message: "이름을 입력해주세요",
-            trigger: "blur",
-          },
-        ],
-        UserBirth: [
-          {
-            required: true,
-            message: "생년월일을 입력해주세요",
-            trigger: "blur",
-          },
-          {
-            min: 6,
-            max: 6,
-            message: "'YYMMDD'형식의 생년월일을 입력해주세요",
-            trigger: "blur",
-          },
-        ],
-        Gender: [
-          {
-            required: true,
-            message: "성별을 체크해주세요",
-            trigger: "change",
-          },
+          { validator: checkPWCF, trigger: "blur" },
         ],
         UserTel: [
           {
@@ -178,7 +152,24 @@ export default {
         if (valid) {
           // alert('submit!');
           this.openFullScreen2();
-          this.$store.state.SignupDialogIndiv = false;
+          // 회원정보 수정
+          axios
+            .put("/ind", {
+              ind_birth: this.ruleForm.UserBirth,
+              ind_email: this.ruleForm.UserEmail,
+              ind_gender: this.ruleForm.Gender,
+              ind_id: this.ruleForm.UserId,
+              ind_index: this.ruleForm.UserIndex,
+              ind_name: this.ruleForm.UserName,
+              ind_password: this.ruleForm.UserEmail,
+              ind_phone: this.ruleForm.UserTel,
+            })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
           this.successmessage();
         } else {
           console.log("error submit!!");
