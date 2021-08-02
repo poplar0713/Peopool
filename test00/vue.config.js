c5282a259ed7c4fd0f95da8277b48c8180042b76
조영우 <awesome_95@naver.com>
2021-08-02 20:03:08 +0900

    no message

--- test00/vue.config.js
+++ test00/vue.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  outputDir: "../peopool/src/main/resources/static",
-  indexPath: "../static/index.html",
+  // outputDir: "../peopool/src/main/resources/static",
+  // indexPath: "../static/index.html",
   devServer: {
     https: true,
-    port: 8443,
+    port: 8444,
     open: true,
     proxy: {
       "api.v1": {
-        target: "https://localhost:8443/",
+        target: "https://52.79.162.52:8443/",
       },
       "/webjars": {
-        target: "https://localhost:8443/",
+        target: "https://52.79.162.52:8443/",
       },
       "/group-call": {
-        target: "https://localhost:8443/",
+        target: "https://52.79.162.52:8443/",
       },
       "/upload": {
-        target: "https://localhost:8443/",
+        target: "https://52.79.162.52:8443/",
       },
       "/auth": {
-        target: "https://localhost:8443/",
+        target: "https://52.79.162.52:8443/",
       },
     },
   },
