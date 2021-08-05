47c463418f765d5dbe9f824db06aa9e74005bceb
Jeongdong <duwjdehd0713@gmail.com>
2021-08-05 10:44:49 +0900

    fmr

--- test00/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    devServer: {
-      https: {
-        key: fs.readFileSync('public/keys/peopool.com.key'),
-        cert: fs.readFileSync('public/keys/peopool.com.crt'),
-        ca: fs.readFileSync('public/keys/peopool.crt'),
-    }
-  }  
-};
