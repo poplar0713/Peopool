0c14b8a956e5e96131c64b7a7abe1a213ed01088
조영우 <awesome_95@naver.com>
2021-08-09 13:38:14 +0900

    tags.vue jwt 주석처리

--- test00/src/components/SideBarComponents/UserProfileInfo/SideBarProfileUserTags.vue
+++ test00/src/components/SideBarComponents/UserProfileInfo/SideBarProfileUserTags.vue
@@ -32,11 +32,11 @@
 
 <script>
 import axios from "axios";
-import jwt_decode from "jwt-decode";
+// import jwt_decode from "jwt-decode";
 // 토큰가져오기
 const token = localStorage.getItem("token");
-const decoded = jwt_decode(token);
-const index = decoded.index;
+// const decoded = jwt_decode(token);
+// const index = decoded.index;
 export default {
   data() {
     // 태그목록 불러오기
@@ -57,26 +57,26 @@ export default {
         }
       });
     // 유저본인 태그목록 불러오기
-    axios
-      .get("https://i5d206.p.ssafy.io:8443/has/tag", {
-        headers: { Authorization: token },
-        params: {
-          index: index,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        this.mytags = res.data;
-      })
-      .catch((err) => {
-        console.log(err.response.data.status);
-        if (err.response.data.status == 401) {
-          console.log("token error");
-          this.$message.error("로그인세션이 만료되었습니다");
-          localStorage.clear();
-          this.$router.push("/");
-        }
-      });
+    // axios
+    //   .get("https://i5d206.p.ssafy.io:8443/has/tag", {
+    //     headers: { Authorization: token },
+    //     params: {
+    //       index: index,
+    //     },
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //     this.mytags = res.data;
+    //   })
+    //   .catch((err) => {
+    //     console.log(err.response.data.status);
+    //     if (err.response.data.status == 401) {
+    //       console.log("token error");
+    //       this.$message.error("로그인세션이 만료되었습니다");
+    //       localStorage.clear();
+    //       this.$router.push("/");
+    //     }
+    //   });
     return {
       // 불러온 태그들
       options: [],
