7409a3941e91b8266be3a7288f1291f2cf386875
DESKTOP-KVCQHCD\multicampus <shchae.dev@gmail.com>
2021-08-09 21:50:41 +0900

    post 수정

--- peopool/src/main/java/com/ssafy/peopool/controller/ProfileOfIndividualController.java
+++ peopool/src/main/java/com/ssafy/peopool/controller/ProfileOfIndividualController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -98,7 +99,7 @@ public class ProfileOfIndividualController {
 	
 	
 	@ApiOperation(value = "index에 해당하는 이력서을 수정한다.", response = String.class)
-	@PutMapping("/resume/{index}")
+	@PostMapping("/resume/{index}")
 	public ResponseEntity<String> modifyResume(@PathVariable("index")int index, @RequestParam("upfile") MultipartFile[] files) throws IllegalStateException, IOException {
 		ProfileOfIndividual profileOfIndividual = new ProfileOfIndividual();
 		if(!files[0].isEmpty()) {
