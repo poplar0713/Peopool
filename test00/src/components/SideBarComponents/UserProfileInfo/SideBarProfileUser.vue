2096159c7251de0929e92f73ec246e69dddef0b2
mooonmovie <sosm8500@gmail.com>
2021-08-12 18:13:50 +0900

    User server resources

--- test00/src/components/SideBarComponents/UserProfileInfo/SideBarProfileUser.vue
+++ test00/src/components/SideBarComponents/UserProfileInfo/SideBarProfileUser.vue
@@ -2,7 +2,9 @@
   <el-tabs :tab-position="tabPosition" style="height: 100%;">
     <el-tab-pane label="기본정보"><SideBarProfileUserInfo /></el-tab-pane>
     <!-- <el-tab-pane label="Level of Education"><SideBarProfileUserEducation /></el-tab-pane> -->
-    <el-tab-pane label="프로필사진 및 소개"><SideBarProfileUserIntroduction /></el-tab-pane>
+    <el-tab-pane label="프로필사진 및 소개"
+      ><SideBarProfileUserIntroduction
+    /></el-tab-pane>
     <el-tab-pane label="소개영상"><PRVideo /></el-tab-pane>
     <el-tab-pane label="태그관리"><SideBarProfileUserTags /></el-tab-pane>
     <el-tab-pane label="서류관리"><SideBarProfileUserDoc /></el-tab-pane>
@@ -17,6 +19,8 @@ import SideBarProfileUserTags from "./SideBarProfileUserTags.vue";
 import PRVideo from "./PRVideo.vue";
 import SideBarProfileUserDoc from "./SideBarProfileUserDoc.vue";
 import DeleteUserAccount from "./DeleteUserAccount.vue";
+import axios from "axios";
+
 export default {
   components: {
     SideBarProfileUserInfo,
@@ -27,8 +31,57 @@ export default {
     DeleteUserAccount,
   },
   data() {
+    const token = this.$cookies.get("PID_AUTH");
+    const decoded = jwt_decode(token);
+    const index = decoded.index;
+    axios
+      .get(`https://i5d206.p.ssafy.io:8443/poi/${this.userindex}`, {
+        headers: { Authorization: token },
+      })
+      .then((res) => {
+        this.userdata.photofilepath =
+          "/file/" + res.data.photo_savefolder + "/" + res.data.photo_savefile;
+        this.userdata.resumefilepath =
+          "/file/" +
+          res.data.resume_savefolder +
+          "/" +
+          res.data.resume_savefile;
+        this.userdata.videofilepath =
+          "/file/" + res.data.video_savefolder + "/" + res.data.video_savefile;
+        this.userdata.resume_originfile = res.data.resume_originfile;
+        this.userdata.photo_originfile = res.data.photo_originfile;
+        this.userdata.video_originfile = res.data.video_originfile;
+        this.userdata.ind_switch = res.data.ind_switch;
+        this.userdata.ind_introduce = res.data.ind_introduce;
+        this.userdata.photo_index = res.data.photo_index;
+        this.userdata.resume_index = res.data.resume_index;
+        this.userdata.video_index = res.data.resume_index;
+        this.userdata.ind_index = res.data.ind_index;
+        this.userdata.ind_name = res.data.ind_name;
+        this.userdata.ind_email = res.data.ind_email;
+        this.userdata.ind_phone = res.data.ind_phone;
+        this.userdata.ind_gender = res.data.ind_gender;
+      });
     return {
       tabPosition: "left",
+      userdata: [
+        { ind_name: "" },
+        { ind_gender: "" },
+        { ind_phone: "" },
+        { ind_email: "" },
+        { photofilepath: "" },
+        { resume_originfile: "" },
+        { photo_index: "" },
+        { resume_index: "" },
+        { video_index: "" },
+        { ind_index: 0 },
+        { video_originfile: "" },
+        { videofilepath: "" },
+        { photo_originfile: "" },
+        { resumefilepath: "" },
+        { ind_switch: "" },
+        { ind_introduce: "" },
+      ],
     };
   },
 };
