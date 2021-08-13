d5ec8423042e96c82a992a87d8cc0b098eb3ee6c
조영우 <awesome_95@naver.com>
2021-08-13 15:27:03 +0900

    usertag처럼 companytag도 바꿈

--- test00/src/components/SideBarComponents/CompanyProfileInfo/SideBarProfileCompanyTags.vue
+++ test00/src/components/SideBarComponents/CompanyProfileInfo/SideBarProfileCompanyTags.vue
@@ -38,7 +38,6 @@
         closable
         :disable-transitions="true"
         @close="handleClose(tag, item.list_index)"
-        @click="GetTagCompany(item.list_name)"
       >
         {{ item.list_name }}
       </el-tag>
@@ -108,7 +107,36 @@ export default {
       value: "",
       //나의 태그들
       mytags: [],
+      mytagLength: this.getMytagLength,
     };
+  },
+  computed: {
+    getMytagLength() {
+      return this.mytags.length;
+    },
+  },
+  watch: {
+    mytagLength: function() {
+      axios
+        .get("https://i5d206.p.ssafy.io:8443/cla/list", {
+          headers: { Authorization: this.token },
+          params: {
+            ent_index: this.index,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          this.mytags = res.data;
+        })
+        .catch((err) => {
+          if (err.response == 401) {
+            console.log("token error");
+            this.$message.error("로그인세션이 만료되었습니다");
+            localStorage.clear();
+            this.$router.push("/");
+          }
+        });
+    },
   },
   methods: {
     plustag() {
@@ -117,7 +145,7 @@ export default {
       console.log(this.company_index);
       const mytaglist = [];
       for (var i in this.mytags) {
-        mytaglist.push(this.mytags[i].taglist_index);
+        mytaglist.push(this.mytags[i].list_index);
       }
       console.log(mytaglist);
       if (mytaglist.includes(this.value)) {
@@ -144,30 +172,11 @@ export default {
           });
       }
     },
-
-    GetTagCompany(keyword) {
-      const loading = this.$loading({
-        lock: true,
-        text: "Loading",
-        spinner: "el-icon-loading",
-        background: "rgba(0, 0, 0, 0.7)",
-      });
-      setTimeout(() => {
-        loading.close();
-        this.$router.push({
-          name: "SearchCompany",
-          query: { keyword: keyword },
-        });
-      }, 2000);
-      setTimeout(() => {
-        location.reload();
-      }, 2005);
-    },
     handleClose(tag, tag_index) {
       console.log(tag);
       console.log(tag_index);
       axios
-        .delete(`https://i5d206.p.ssafy.io:8443/has/${tag_index}`, {
+        .delete(`https://i5d206.p.ssafy.io:8443/cla/${tag_index}`, {
           headers: { Authorization: this.token },
         })
         .then(() => {
