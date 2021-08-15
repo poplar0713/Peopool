b04eaa09365916bbf2e0c6a5d86589a9180e93bf
mooonmovie <sosm8500@gmail.com>
2021-08-15 17:58:50 +0900

    async await

--- test00/src/views/ProfileUser.vue
+++ test00/src/views/ProfileUser.vue
@@ -38,13 +38,16 @@
           /></el-tab-pane>
           <el-tab-pane label="서류관리">
             <SideBarProfileUserDoc />
-            <el-button type="warning" round @click="test">show</el-button>
+
             <div v-if="userdata.resume_index == ''" class="fileDoc">
               {{ this.resume_index }}등록된 이력서 및 포트폴리오가 없습니다.
             </div>
             <div v-else id="webviewer">
-              {{ userdata.resumefilepath }}
-              <el-button type="warning" round @click="testreload"
+              <el-button
+                type="warning"
+                round
+                @click="testreload"
+                style="margin:20px"
                 >나의 이력서 보기</el-button
               >
               <webviewer :initialDoc="userdata.resumefilepath" v-if="show" />
@@ -85,44 +88,44 @@ export default {
     DeleteUserAccount,
     webviewer,
   },
-  beforeMount() {
-    // this.userdataload();
-  },
+  beforeMount() {},
   created() {
-    const token = this.$cookies.get("PID_AUTH");
-    const decoded = jwt_decode(token);
-    const index = decoded.index;
-    axios
-      .get(`https://i5d206.p.ssafy.io:8443/poi/${index}`, {
-        headers: { Authorization: token },
-      })
-      .then((res) => {
-        var result = res.data[0];
+    this.userdataload();
+    console.log("userdata ind introduce: ", this.userdata.ind_introduce);
+    // const token = this.$cookies.get("PID_AUTH");
+    // const decoded = jwt_decode(token);
+    // const index = decoded.index;
+    // axios
+    //   .get(`https://i5d206.p.ssafy.io:8443/poi/${index}`, {
+    //     headers: { Authorization: token },
+    //   })
+    //   .then((res) => {
+    //     var result = res.data[0];
 
-        this.userdata.photofilepath =
-          "/file/" + result.photo_savefolder + "/" + result.photo_savefile;
-        this.userdata.resumefilepath =
-          "/file/" + result.resume_savefolder + "/" + result.resume_savefile;
-        console.log(this.userdata.resumefilepath);
-        this.userdata.videofilepath =
-          "/file/" + result.video_savefolder + "/" + result.video_savefile;
-        this.userdata.resume_originfile = result.resume_originfile;
-        this.userdata.photo_originfile = result.photo_originfile;
-        this.userdata.video_originfile = result.video_originfile;
-        this.userdata.ind_switch = result.ind_switch;
-        this.userdata.ind_introduce = result.ind_introduce;
-        this.userdata.photo_index = result.photo_index;
-        this.userdata.resume_index = result.resume_index;
-        this.userdata.video_index = result.resume_index;
-        this.userdata.ind_index = result.ind_index;
-        this.userdata.ind_name = result.ind_name;
-        this.userdata.ind_email = result.ind_email;
-        this.userdata.ind_phone = result.ind_phone;
-        this.userdata.ind_gender = result.ind_gender;
-        this.testurl =
-          "https://i5d206.p.ssafy.io" + this.userdata.resumefilepath;
-        console.log("this userintroduce: ", this.userdata.ind_introduce);
-      });
+    //     this.userdata.photofilepath =
+    //       "/file/" + result.photo_savefolder + "/" + result.photo_savefile;
+    //     this.userdata.resumefilepath =
+    //       "/file/" + result.resume_savefolder + "/" + result.resume_savefile;
+    //     console.log(this.userdata.resumefilepath);
+    //     this.userdata.videofilepath =
+    //       "/file/" + result.video_savefolder + "/" + result.video_savefile;
+    //     this.userdata.resume_originfile = result.resume_originfile;
+    //     this.userdata.photo_originfile = result.photo_originfile;
+    //     this.userdata.video_originfile = result.video_originfile;
+    //     this.userdata.ind_switch = result.ind_switch;
+    //     this.userdata.ind_introduce = result.ind_introduce;
+    //     this.userdata.photo_index = result.photo_index;
+    //     this.userdata.resume_index = result.resume_index;
+    //     this.userdata.video_index = result.resume_index;
+    //     this.userdata.ind_index = result.ind_index;
+    //     this.userdata.ind_name = result.ind_name;
+    //     this.userdata.ind_email = result.ind_email;
+    //     this.userdata.ind_phone = result.ind_phone;
+    //     this.userdata.ind_gender = result.ind_gender;
+    //     this.testurl =
+    //       "https://i5d206.p.ssafy.io" + this.userdata.resumefilepath;
+    //     console.log("this userintroduce: ", this.userdata.ind_introduce);
+    //   });
   },
   mounted() {
     console.log(server_url);
@@ -133,6 +136,7 @@ export default {
     const decoded = jwt_decode(token);
     const index = decoded.index;
     // 회원정보 가져오기
+
     axios
       .get(`https://i5d206.p.ssafy.io:8443/ind/${index}`, {
         headers: { Authorization: token },
@@ -173,7 +177,7 @@ export default {
           this.$router.push("/");
         }
       });
-
+    setTimeout(() => {}, 2000);
     return {
       username: "",
       mytags: [],
@@ -218,52 +222,47 @@ export default {
           if (response.status == 200) {
             alert("업로드 되었습니다!");
           }
-          setTimeout(() => {
-            this.userdataload();
-            this.show = false;
-          }, 2000);
-          console.log("reload resume: ", this.userdata.resumefilepath);
+          this.test();
         })
         .catch((error) => {
           console.log(error);
         });
     },
     async userdataload() {
+      alert("async await");
       const token = this.$cookies.get("PID_AUTH");
       const decoded = jwt_decode(token);
       const index = decoded.index;
-      const res = await axios.get(
-        `https://i5d206.p.ssafy.io:8443/poi/${index}`,
-        {
-          headers: { Authorization: token },
-        }
-      );
-
-      var result = res.data[0];
-      console.log("load ", result);
-      this.userdata.photofilepath =
-        "/file/" + result.photo_savefolder + "/" + result.photo_savefile;
-      console.log(this.userdata.photofilepath);
-      // this.userdata.resumefilepath = "/docx_pdf/test.pdf";
-      this.userdata.resumefilepath =
-        "/file/" + result.resume_savefolder + "/" + result.resume_savefile;
-      this.userdata.videofilepath =
-        "/file/" + result.video_savefolder + "/" + result.video_savefile;
-      this.userdata.resume_originfile = result.resume_originfile;
-      this.userdata.photo_originfile = result.photo_originfile;
-      this.userdata.video_originfile = result.video_originfile;
-      this.userdata.ind_switch = result.ind_switch;
-      this.userdata.ind_introduce = result.ind_introduce;
-      this.userdata.photo_index = result.photo_index;
-      this.userdata.resume_index = result.resume_index;
-      this.userdata.video_index = result.resume_index;
-      this.userdata.ind_index = result.ind_index;
-      this.userdata.ind_name = result.ind_name;
-      this.userdata.ind_email = result.ind_email;
-      this.userdata.ind_phone = result.ind_phone;
-      this.userdata.ind_gender = result.ind_gender;
-
-      console.log("userdata ind introduce: ", this.userdata.ind_introduce);
+      try {
+        const res = await axios.get(
+          `https://i5d206.p.ssafy.io:8443/poi/${index}`,
+          {
+            headers: { Authorization: token },
+          }
+        );
+        var result = res.data[0];
+        this.userdata.photofilepath =
+          "/file/" + result.photo_savefolder + "/" + result.photo_savefile;
+        this.userdata.resumefilepath =
+          "/file/" + result.resume_savefolder + "/" + result.resume_savefile;
+        this.userdata.videofilepath =
+          "/file/" + result.video_savefolder + "/" + result.video_savefile;
+        this.userdata.resume_originfile = result.resume_originfile;
+        this.userdata.photo_originfile = result.photo_originfile;
+        this.userdata.video_originfile = result.video_originfile;
+        this.userdata.ind_switch = result.ind_switch;
+        this.userdata.ind_introduce = result.ind_introduce;
+        this.userdata.photo_index = result.photo_index;
+        this.userdata.resume_index = result.resume_index;
+        this.userdata.video_index = result.resume_index;
+        this.userdata.ind_index = result.ind_index;
+        this.userdata.ind_name = result.ind_name;
+        this.userdata.ind_email = result.ind_email;
+        this.userdata.ind_phone = result.ind_phone;
+        this.userdata.ind_gender = result.ind_gender;
+      } catch (error) {
+        console.log(error);
+      }
     },
     uploadFile() {},
     handleRemove(file, fileList) {
@@ -282,7 +281,18 @@ export default {
       this.show = !this.show;
     },
     test() {
+      const loading = this.$loading({
+        lock: true,
+        text: "Loading",
+        spinner: "el-icon-loading",
+        background: "rgba(0, 0, 0, 0.7)",
+      });
       this.show = false;
+      this.userdataload();
+      setTimeout(() => {
+        loading.close();
+      }, 2000);
+      this.show = true;
     },
   },
 };
